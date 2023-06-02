af22e16440de4691c0e71fb6449d6667c0f84224
unknown <chioujryu@gmail.com>
2023-06-02 13:16:25 +0800

    2023.06.02.13.16

--- helper_functions_generic.py
+++ helper_functions_generic.py
@@ -1,31 +1,9 @@
 ### We create a bunch of helpful functions throughout the course.
 ### Storing them here so they're easily accessible.
 
-import tensorflow as tf
 
-# Create a function to import an image and resize it to be able to be used with our model
-def load_and_prep_image(filename, img_shape=224, scale=True):
-  """
-  Reads in an image from filename, turns it into a tensor and reshapes into
-  (224, 224, 3).
 
-  Parameters
-  ----------
-  filename (str): string filename of target image
-  img_shape (int): size to resize target image to, default 224
-  scale (bool): whether to scale pixel values to range(0, 1), default True
-  """
-  # Read in the image
-  img = tf.io.read_file(filename)
-  # Decode it into a tensor
-  img = tf.image.decode_jpeg(img)
-  # Resize the image
-  img = tf.image.resize(img, [img_shape, img_shape])
-  if scale:
-    # Rescale the image (get all values between 0 and 1)
-    return img/255.
-  else:
-    return img
+
 
 # Note: The following confusion matrix code is a remix of Scikit-Learn's 
 # plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
@@ -114,48 +92,10 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   if savefig:
     fig.savefig("confusion_matrix.png")
   
-# Make a function to predict on images and plot them (works with multi-class)
-def pred_and_plot(model, filename, class_names):
-  """
-  Imports an image located at filename, makes a prediction on it with
-  a trained model and plots the image with the predicted class as the title.
-  """
-  # Import the target image and preprocess it
-  img = load_and_prep_image(filename)
 
-  # Make a prediction
-  pred = model.predict(tf.expand_dims(img, axis=0))
-
-  # Get the predicted class
-  if len(pred[0]) > 1: # check for multi-class
-    pred_class = class_names[pred.argmax()] # if more than one output, take the max
-  else:
-    pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
-
-  # Plot the image and predicted class
-  plt.imshow(img)
-  plt.title(f"Prediction: {pred_class}")
-  plt.axis(False);
-  
 import datetime
 
-def create_tensorboard_callback(dir_name, experiment_name):
-  """
-  Creates a TensorBoard callback instand to store log files.
 
-  Stores log files with the filepath:
-    "dir_name/experiment_name/current_datetime/"
-
-  Args:
-    dir_name: target directory to store TensorBoard log files
-    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
-  """
-  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-  tensorboard_callback = tf.keras.callbacks.TensorBoard(
-      log_dir=log_dir
-  )
-  print(f"Saving TensorBoard log files to: {log_dir}")
-  return tensorboard_callback
 
 # Plot the validation and training data separately
 import matplotlib.pyplot as plt
